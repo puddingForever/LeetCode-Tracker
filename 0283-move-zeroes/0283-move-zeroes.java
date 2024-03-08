8dbaaa7b55f711d25e0773ce7ef81a66c0b318f0
BytePudding <126591306+puddingForever@users.noreply.github.com>
2024-03-08 13:41:55 +0900

    Time: 1 ms (100.00%), Space: 46.2 MB (40.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,17 +3,16 @@ class Solution {
       //shift all the numbers to front
      //fill up the rest with 0 
         
-     int posNum = 0 ;
-        
+     int posNum = 0;
+     
      for(int num : nums){
-         if(num != 0) nums[posNum++] = num; 
+         if(num != 0) nums[posNum++] = num;
      }
         
      while(posNum < nums.length){
-         nums[posNum] = 0; 
+         nums[posNum] = 0;
          posNum++;
      }
-        
         
     }
 }
