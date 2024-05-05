073191efc619e18da1d6b65df7c902f376b8720e
BytePudding <126591306+puddingForever@users.noreply.github.com>
2024-05-05 14:58:43 +0900

    Time: 0 ms (100.00%), Space: 45.9 MB (20.77%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,13 +1,11 @@
 class Solution {
     public int search(int[] nums, int target) {
         
-        int left = 0 ;
+        int left = 0;
         int right = nums.length-1;
-       
-        while(left<=right){
-            
-            int mid = left + (right - left) / 2 ;
-            
+        
+        while(left <= right){
+            int mid = left + (right-left) / 2;
             if(nums[mid] == target){
                 return mid;
             }
@@ -17,11 +15,9 @@ class Solution {
             }else{
                 right = mid - 1;
             }
+            
         }
         
         return -1;
-        
-        
-        
     }
 }
