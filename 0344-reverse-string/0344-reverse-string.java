aa72617fb1fba23f924560af9e73c5e6a629ec39
BytePudding <126591306+puddingForever@users.noreply.github.com>
2024-05-05 13:34:42 +0900

    Time: 0 ms (100.00%), Space: 49.3 MB (26.15%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,18 +1,17 @@
 class Solution {
     public void reverseString(char[] s) {
-                
-      int i = 0;
-      int j = s.length-1;
         
-      while(i < j){
-          
-          char temp = s[i];
-          s[i] = s[j];
-          s[j] = temp;
-          i++;
-          j--;
-      }
+       int left = 0 ; 
+       int right = s.length-1;
+        
+       while(left < right){
+         char tmp = s[left];
+         s[left] = s[right];
+         s[right] = tmp;
+          left++;
+          right--;
+       }
+        
         
     }
 }
-
