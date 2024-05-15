fac390761d98e39fabc12d01dd8fe21882077c1f
PuddingForever <developerjieun94@gmail.com>
2024-05-15 09:44:27 +0900

    Time: 209 ms (5.00%), Space: 45.8 MB (78.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int point = 0;
         
-        //shift all the elements to front
-        int posNum = 0;
-        
-        for(int num : nums){
-            if(num != 0) nums[posNum++] = num;
+        for(int i=0; i<nums.length-1; i++){
+          for(int j=i+1; j<nums.length; j++){
+              if(nums[i] == 0){
+                  int temp = nums[i];
+                  nums[i] = nums[j];
+                  nums[j] = temp;
+              }
+          }
         }
-        
-        //fill the left elements to 0 
-        while(posNum<nums.length){
-            nums[posNum++] = 0;
-        }
-        
-        
         
     }
 }
