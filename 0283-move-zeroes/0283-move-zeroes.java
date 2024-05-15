27b57aca7e723103d3d3a51f25fc806f816eb070
PuddingForever <developerjieun94@gmail.com>
2024-05-15 10:08:59 +0900

    Time: 5 ms (14.21%), Space: 45.4 MB (97.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,15 +1,22 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int point = 0;
+    public void moveZeroes(int[] nums) {     
         
-        for(int i=0; i<nums.length-1; i++){
-          for(int j=i+1; j<nums.length; j++){
-              if(nums[i] == 0){
-                  int temp = nums[i];
-                  nums[i] = nums[j];
-                  nums[j] = temp;
-              }
-          }
+       if(nums.length == 1) return;
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0)
+                queue.add(nums[i]);
+        }
+        
+        int index = 0;
+        while(queue.size() != 0){
+            nums[index] = queue.poll();
+            index++;
+        }
+        
+        for(int i=index; i<nums.length; i++){
+            nums[i] = 0;
         }
         
     }
