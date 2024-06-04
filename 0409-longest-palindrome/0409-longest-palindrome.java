9a7f74de0945f3259168afe21052403fca533d0d
PuddingForever <developerjieun94@gmail.com>
2024-06-04 20:35:59 +0900

    Time: 7 ms (53.74%), Space: 42.2 MB (12.05%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,19 +1,20 @@
-class Solution { // a c
-    // count 2
+class Solution { 
     public int longestPalindrome(String s) {
-      if(s == null || s.length()==0) return 0;
-      HashSet<Character> hs = new HashSet<Character>();
-      int count = 0;
-      for(int i=0; i<s.length(); i++){ // a b b b c 
-          if(hs.contains(s.charAt(i))){
-              hs.remove(s.charAt(i)); // 
-              count++; // 1 
-          }else{
-              hs.add(s.charAt(i)); 
-          }
-      }
+     
+        HashSet<Character> hs = new HashSet();
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(hs.contains(s.charAt(i))){
+                hs.remove(s.charAt(i));
+                count++;
+            }else{
+                 hs.add(s.charAt(i));
+            }    
+        }
         
-     if(!hs.isEmpty()) return count*2 + 1;
-     return count*2;
+        if(!hs.isEmpty()) return count*2+1;
+
+        return count * 2;
+        
     }
 }
