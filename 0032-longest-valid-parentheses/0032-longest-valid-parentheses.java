19a663b78f5313d77ae8f185e704c706e4825cf7
PuddingForever <developerjieun94@gmail.com>
2024-08-03 23:46:04 +0900

    Time: 3 ms (69.64%), Space: 42.8 MB (51.39%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -1,29 +1,43 @@
 class Solution {
     public int longestValidParentheses(String s) {
         
-      
-        // remove from the stack when a paranthesis is closing tag
-           // if current stack gets empty , store current index instead of calculating 
-           // calculate the length (base)-(currentIndex) 
-        // store into the stack when a paranthesis is opening tag
+       // (()) => 4 
+       // (() => 2 
         
-        Stack<Integer> mySt = new Stack();
-        mySt.push(-1);
+        // get the longest valid parentheses
+                
+        // store in a stack when the value is ( <-- 
+        // when meets ending tag , remove it from the stack 
+        
+//        0 1 2 3 4 5 
+//         == 1 2 2 
+        
+        
+        // )()()
+        
+        // 
+        
+        Stack<Integer> myStack = new Stack();
+        myStack.push(-1); // prevent when the stack is empty 
         int ans = 0;
+        
         for(int i=0; i<s.length(); i++){
             
             if(s.charAt(i) == '('){
-                mySt.push(i);
-            }else{
-                mySt.pop();
-                if(mySt.size() == 0){
-                    mySt.push(i);
-                }else{
-                    ans = Math.max(ans,i - mySt.peek());  
-                }   
+                myStack.push(i);   // 0 3
+            }else if(s.charAt(i) == ')'){
+                myStack.pop(); 
+                if(myStack.size() == 0 ){
+                    myStack.push(i); // 0
+                }else{ // ans =  2-1 = 1
+                    ans = Math.max(ans,i-myStack.peek()); 
+                }
             }
+            
         }
         
+        
         return ans;
+       
     }
 }
