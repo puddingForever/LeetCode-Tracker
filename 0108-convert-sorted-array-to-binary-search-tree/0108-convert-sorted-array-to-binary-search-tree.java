9f402903b8676fdaaddf27c92185984b31d7ff1e
PuddingForever <developerjieun94@gmail.com>
2024-08-09 20:36:08 +0900

    Time: 0 ms (100.00%), Space: 43.3 MB (21.70%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.java
@@ -22,12 +22,7 @@ class Solution {
              left           right       
        (less than middle)  (bigger than middle)
         */
-        
-        
-        // 1. recursion 
-        // 1.1 middle from the array to be the root  (already sorted, no sorting required )
-        // 1.2 recursion to both left and right (check the middle and position to the )
-        // 1.3 iterate left first then right .. 
+
         
         int left = 0; 
         int right = nums.length - 1; // 4
@@ -37,23 +32,24 @@ class Solution {
     }    
     
     //  [-10,-3,0,5,9]
-    public TreeNode sortBST(int left,int right,int[] nums){  
+    public TreeNode sortBST(int left,int right,int[] nums){  // 
         
         if(left > right) return null; 
         
-        // asign root 
-        int mid = left + (right-left) / 2; // nums[2] nums[0] nums[3]
-        TreeNode root = new TreeNode(nums[mid]);  // root : 0  -10  5 
+        // assign root 
+        int mid = left + (right-left) / 2;  //
+        TreeNode root = new TreeNode(nums[mid]);  
      
         /*
-        [0,-10, 5  ]
-        
-          [   ,-3,   ,  ,9
+          [-10,-3,0,5,9] 
+          
+         
+         
     
         */
                             
-        root.left = sortBST(left,mid-1,nums);  //  
-        root.right =  sortBST(mid+1,right,nums); //  3, 4 
+        root.left = sortBST(left,mid-1,nums);  // 0 1
+        root.right =  sortBST(mid+1,right,nums); // 1 4 
         
         return root;
         
