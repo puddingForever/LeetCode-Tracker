98df12cba3cea4267bf24c9be11ba6aad14a8ae9
PuddingForever <developerjieun94@gmail.com>
2025-01-04 11:28:28 +0900

    Time: 54 ms (44.23%), Space: 48.3 MB (98.86%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -4,11 +4,6 @@
  */
 var isEmpty = function(obj) {
 
-    var cnt = 0;
-
-   for(var key in obj){
-        cnt++;
-   }
-
-   return cnt === 0 ? true : false;
+   for(const _ in obj) return false;
+   return true;
 };
