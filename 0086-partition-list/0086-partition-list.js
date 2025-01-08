459db99ffbd0ce2a8fed46da1420828149ded6f8
PuddingForever <developerjieun94@gmail.com>
2025-01-08 21:44:02 +0900

    Time: 0 ms (100%), Space: 51.1 MB (73.48%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function(head, x) {
+    if(!head) return head;
+    var dummy1 = new ListNode(0);
+    var dummy2 = new ListNode(0);
+    var temp = head;
+    var prev1 = dummy1;
+    var prev2 = dummy2;
+
+    while(temp){
+        if(temp.val < x){
+            prev1.next = temp;
+            prev1 = prev1.next;
+        }else{
+            prev2.next = temp;
+            prev2 = prev2.next;
+        }
+        temp = temp.next;
+    }
+
+    prev2.next = null;
+    prev1.next = dummy2.next;
+    head = dummy1.next;
+    return head;
+};
