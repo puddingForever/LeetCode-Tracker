4850cecf52fb9cdb1dc70c75d8f0fe2e25922c62
PuddingForever <developerjieun94@gmail.com>
2025-01-10 08:37:44 +0900

    Time: 49 ms (99%), Space: 53 MB (58.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,13 +11,14 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-
     var slow = head;
     var fast = head;
     while(fast && fast.next){
-        slow = slow.next;
         fast = fast.next.next;
-        if(slow === fast) return true;
+        slow = slow.next;
+        if(fast === slow) return true;
     }
+
     return false;
+   
 };
