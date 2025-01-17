4790d2c69a7b44c4ed36d2295c446cd4cc9c4477
PuddingForever <developerjieun94@gmail.com>
2025-01-17 11:38:15 +0900

    Time: 23 ms (36.4%), Space: 73.4 MB (55.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,30 +10,30 @@
  * @return {boolean}
  */
 var isPalindrome = function(head) {
-    var stackArr = [];
-    var slow = head;
-    var fast = head;
+        if(!head.next) return true;
+     var slow = head;
+     var fast = head;
+     var stack = [];
+   while(fast && fast.next){
+	stack.push(slow.val);
+            slow = slow.next;
+	fast = fast.next.next;
+   }
 
-    while(fast && fast.next){
-        stackArr.push(slow.val);
-        slow = slow.next;
-        fast = fast.next.next;
-    }
 
-    if(fast){
-        slow = slow.next;
-    }
+   if(fast){
+	slow = slow.next;
+   }
 
-    var cnt = stackArr.length-1;
+  var cnt = stack.length-1;
+ 
+ while(slow){
+     if(stack[cnt] !== slow.val){
+        return false;
+     } 
+     slow = slow.next;
+     cnt--;
+  }
 
-    while(slow){
-        var stack = stackArr[cnt];
-        if(stack !== slow.val){
-            return false;
-        }
-        slow = slow.next;
-        cnt--;
-    }
-
-    return true;
+  return true;   
 };
