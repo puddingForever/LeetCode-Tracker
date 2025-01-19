938b1099b731e9906897a9f7fb4a4c7cda8b6b55
PuddingForever <developerjieun94@gmail.com>
2025-01-19 18:26:17 +0900

    Time: 0 ms (100%), Space: 56.4 MB (58.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,14 +3,15 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) { 
-    const myStack = [];
-    for(const chr of s){
-        myStack.push(chr);
-    }
-    let cnt = 0;
-    for(var i=myStack.length-1; i>=0; i--){
-        s[cnt] = myStack[i];
-        cnt++;
-    }
+   
+   let left=0,right=s.length-1;
+
+   while(left < right){
+    let temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+    left++;
+    right--;
+   }
     
 };
