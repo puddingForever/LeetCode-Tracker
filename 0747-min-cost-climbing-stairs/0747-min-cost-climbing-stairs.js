ab21d69a4caefe37ba067c4f9b9f11e5db29bd2a
PuddingForever <developerjieun94@gmail.com>
2025-02-22 21:00:06 +0900

    Time: 2 ms (55.21%), Space: 54.3 MB (5.21%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -3,23 +3,35 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-   
 
-    // to reach top 
-    // dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+    /* the problem */ 
+    // the problem is about climbing moutain where each step has a cost 
+    // my goal is to reach the top with the minimum cost 
+   
+    /* approch */
+    // i used the dynamic programming 
+    // i made an array for storing minimum cost for each steps 
+    // to reach to the exact step , i made this relation 
+    //  dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+    // it means to reach step i , we take the minimum cost between step i-1 and i-2 , and add the current step
+
+    // Implementation 
+
+   // I created a dp array where we can store the minmum cost for each step  
     const lastIndex = cost.length;
     const dp = new Array(lastIndex).fill(0);
 
-    // the first step does not cost anything . it is just to initialize the  cost 
+    // the first step and second step does not cost anything so i initialized the base cases. 
     dp[0] = cost[0];
     dp[1] = cost[1];
 
-    // start calculating from the index 2 
-     // [10,15,20] ,  30 
+    // we iterate from step 2 to the last step 
     for(let i=2; i<lastIndex; i++){
-        dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i] ;
+            // i used the formula 
+        dp[i] = Math.min(dp[i-1], dp[i-2]) + cost[i] ;  
     }
 
+    // since we can finish at the last step or the second-to-last step , return with math.min method. 
     return  Math.min(dp[lastIndex-2],dp[lastIndex-1]);
 };
 
