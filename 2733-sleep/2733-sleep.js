d0ea993141513be9b673cda6b67c5e3bbd8fddb9
PuddingForever <developerjieun94@gmail.com>
2025-03-18 13:42:15 +0900

    Time: 57 ms (19.04%), Space: 53.8 MB (8.86%) - LeetHub

---
+++ 2733-sleep/2733-sleep.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+   let myPromise = new Promise(resolve => setTimeout(resolve,millis) )
+
+   return await myPromise
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
