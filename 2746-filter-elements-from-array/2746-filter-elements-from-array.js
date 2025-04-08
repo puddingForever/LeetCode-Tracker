8725a1568ba04a21c5fe2e4b04c32faa20f63956
PuddingForever <developerjieun94@gmail.com>
2025-04-08 10:05:05 +0900

    Time: 49 ms (29.64%), Space: 53.1 MB (67.98%) - LeetHub

--- 2746-filter-elements-from-array/2746-filter-elements-from-array.js
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -4,13 +4,7 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let res = [];
-
-    for(let i=0; i<arr.length; i++){
-        if(fn(arr[i],i)) res.push(arr[i])
-    }
-
-    return res;
-
+   
+   return arr.filter(fn);
     
 };
