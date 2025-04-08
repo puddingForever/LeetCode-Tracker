8ef173988e5a197ec5a3fc80875431d2583dfcc8
PuddingForever <developerjieun94@gmail.com>
2025-04-08 09:17:29 +0900

    Time: 6 ms (68.06%), Space: 55.8 MB (81.44%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.js
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.js
@@ -4,31 +4,19 @@
  * @return {number[]}
  */
 var findIntersectionValues = function(nums1, nums2) {    
-   let nums1Obj = {};
-    let nums2Obj = {};
-    let num1Cnt = 0
-    let num2Cnt = 0;
+    
+    let res1 = 0;
+    let res2 = 0;
 
    for(let i=0; i<nums1.length; i++){
-     nums1Obj[nums1[i]] = true;
+     if(nums2.includes(nums1[i])) res1++;
    }
 
     for(let i=0; i<nums2.length; i++){
-     nums2Obj[nums2[i]] = true;
+      if(nums1.includes(nums2[i])) res2++;
    }
 
-  for(let i=0; i<nums2.length; i++){
-     if(nums1Obj[nums2[i]]){
-        num2Cnt++; 
-     }
-   }
-
-    for(let i=0; i<nums1.length; i++){
-        if(nums2Obj[nums1[i]]){
-            num1Cnt++; 
-        }
-   }
-
-    return [num1Cnt, num2Cnt];
+ 
+    return [res1, res2];
 
 }
