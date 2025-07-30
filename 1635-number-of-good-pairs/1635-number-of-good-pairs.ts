8dedb47c737ec4b39130c457665c7776456ae29f
BytePudding <developerjieun94@gmail.com>
2025-07-30 13:17:23 +0900

    Time: 0 ms (100%), Space: 55.2 MB (62.22%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.ts
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.ts
@@ -1,19 +1,14 @@
 function numIdenticalPairs(nums: number[]): number {
-     if(nums.length === 1) return 0;
+    let goodpair = 0;
+    let obj = {};
 
-    let count = 0, res = 0;   
- 
-
-    for(let i=0; i < nums.length; i++){
-	count = i + 1;
-	while(count < nums.length){
-	       if(nums[i] === nums[count]){
-	           res++;
-                   }
-	    count++;
-	}	
-    }   
-
-   return res;
-
+    for(let num of nums){
+        if(obj[num]){
+            goodpair += obj[num]
+            obj[num]++;
+        }else{
+            obj[num] = 1;
+        }
+    }
+    return goodpair;
 };
