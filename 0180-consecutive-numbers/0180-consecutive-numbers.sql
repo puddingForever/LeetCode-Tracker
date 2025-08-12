4ce2284e2fbda07c53c21cd26d05c3a0162c9f37
BytePudding <developerjieun94@gmail.com>
2025-08-12 11:09:15 +0900

    Time: 520 ms (92.36%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,22 +1,10 @@
-with cte1 as(
-    select id
-         , num 
-         , row_number() over (order by id) as rn_all -- 원래 순서 
-         , row_number() over (partition by num order by id ) as rn_grp -- num 그룹내의 순서 
-    from Logs 
-),
-cte2 as (
-    select num 
-        , rn_all- rn_grp as diff 
-      from cte1
-),
-cte3 as (
-    select num 
-         , diff
-         , count(*) as cnt 
-      from cte2 
-      group by num, diff 
-      having count(*) >= 3  
-)
 select distinct num as ConsecutiveNums 
-  from cte3; 
+from (
+    select id 
+        , num 
+        , LAG(num,1,0) over (order by id) as prev1
+        , LAG(num,2,0) over (order by id) as prev2 
+    from LOGS 
+)
+where num = prev1 and num = prev2;
+
