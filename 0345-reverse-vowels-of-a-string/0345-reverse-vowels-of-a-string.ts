cdda85d2a7423f42f004cb8dc19f81a6cbf577c9
BytePudding <developerjieun94@gmail.com>
2025-08-21 13:24:19 +0900

    Time: 6 ms (78.48%), Space: 61.7 MB (69.51%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -1,34 +1,24 @@
 function reverseVowels(s: string): string {
 
-     let arr = [];
-    let str = "";
-    for(let i=0; i<s.length; i++){
-        let singleChar = s[i].toLowerCase();
-        if(singleChar === 'a' ||
-           singleChar === 'e' ||
-          singleChar === 'i' ||
-          singleChar === 'o' ||
-          singleChar === 'u'){
-            arr.push(s[i]);
-          }
-    }
+    const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']); 
+    let arr = s.split(""); 
+    let start = 0;
+    let end = arr.length - 1;
 
+    while(start < end){
+        // if they are vowels, break and swap 
+        while(start < end && !vowels.has(arr[start])){
+            start++;
+        }
 
-    for(let i=0; i<s.length; i++){
-        let singleChar = s[i].toLowerCase();
-        if(singleChar === 'a' ||
-           singleChar === 'e' ||
-          singleChar === 'i' ||
-          singleChar === 'o' ||
-          singleChar === 'u'){
-              str += arr.pop();
-          }else{
-            str += s[i];
-          }
+         while(start < end && !vowels.has(arr[end])){
+            end--;
+        }
+
+        [arr[start],arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;   
     }
-    
-    
-    return str;
-    
+    return arr.join("")
     
 };
