0899800816c5dbbaa3c33bd32d8c83f1ab8abfad
BytePudding <developerjieun94@gmail.com>
2025-08-21 10:28:29 +0900

    Time: 1 ms (84.85%), Space: 57.6 MB (80.02%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -0,0 +1,16 @@
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+
+
+ for(let i=0; i<flowerbed.length; i++){
+      if(flowerbed[i] === 0 && 
+         (i === 0 || flowerbed[i-1] === 0) && 
+         ( i === flowerbed.length - 1 || flowerbed[i+1] === 0)){
+         n--
+          flowerbed[i] = 1;
+         if(n === 0 ) return true;
+      }
+  }
+
+  return n <=0 ;
+    
+};
