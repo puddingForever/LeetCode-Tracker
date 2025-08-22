7d52659d15f18bced21173711208f8260812baa7
BytePudding <developerjieun94@gmail.com>
2025-08-22 14:53:03 +0900

    Time: 1 ms (84.72%), Space: 58.2 MB (41.11%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.ts
+++ 0605-can-place-flowers/0605-can-place-flowers.ts
@@ -1,26 +1,16 @@
 
 function canPlaceFlowers(flowerbed: number[], n: number): boolean {
-   for(let i=0; i<flowerbed.length; i++){
-        if(i === 0 && flowerbed[i] === 0 ){ // first element 
-            if(flowerbed.length > 1 && flowerbed[i+1] === 0 ){
-                   flowerbed[i] = 1;
-                  n--;
-            }else if(flowerbed.length === 1){
-                 flowerbed[i] = 1;
-                  n--;
-            }
-        }else if(i !==0 && flowerbed[i] === 0
-                        && flowerbed[i+1] === 0 
-                        && flowerbed[i-1] === 0){
-            flowerbed[i] = 1;
-            n--;
-        }else if( i === flowerbed.length-1 && flowerbed[i-1] === 0
-                                           && flowerbed[i] === 0){
-            flowerbed[i] = 1;
-            n--;
+    for(let i=0; i<flowerbed.length; i++){
+       if(flowerbed[i] === 0 &&
+          (i === 0 || flowerbed[i-1] === 0 ) &&
+          (i === flowerbed.length - 1 || flowerbed[i+1] === 0)){
+           flowerbed[i] = 1;
+           n--;
+        if(n === 0) return true;
         }
     }
-   return n<=0;
+
+    return n <= 0;
     
 };
 
