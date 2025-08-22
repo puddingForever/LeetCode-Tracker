9fee3b698b3b38432edc42d8c05f3ad1a677bdd6
BytePudding <developerjieun94@gmail.com>
2025-08-22 11:07:16 +0900

    Time: 0 ms (100%), Space: 55.1 MB (89.56%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.ts
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -1,11 +1,9 @@
 function isSubsequence(s: string, t: string): boolean {
     
-    let count = 0; // ab
+    let cnt = 0;
     for(let i=0; i<t.length; i++){
-        if(t[i] === s[count]){
-            count++; 
-        }
+        if(s[cnt] === t[i]) cnt++;
     }
 
-    return s.length === count;
+    return cnt === s.length
 };
