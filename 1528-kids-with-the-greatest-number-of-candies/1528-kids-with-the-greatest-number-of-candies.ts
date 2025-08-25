c740dec55fb36871f4bda68195c2a373d3e6eaf7
BytePudding <developerjieun94@gmail.com>
2025-08-25 17:39:55 +0900

    Time: 0 ms (100%), Space: 55.8 MB (80.43%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.ts
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.ts
@@ -1,4 +1,7 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-   let maxKid = Math.max(...candies);
-   return candies.map(candy => candy + extraCandies >= maxKid );
-};
+    // find a kid who has most candies 
+   let maxKid = Math.max(...candies); 
+
+   // check if a kid can exceed the one with the maximum candies
+   return candies.map(candy => candy + extraCandies >= maxKid ); 
+}
