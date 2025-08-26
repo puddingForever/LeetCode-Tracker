4daf2baae95640a301d5667dfbfe5ca1582b1744
BytePudding <developerjieun94@gmail.com>
2025-08-26 14:02:33 +0900

    Time: 2 ms (30.59%), Space: 56.4 MB (27.77%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.ts
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.ts
@@ -1,10 +1,14 @@
 function uniqueOccurrences(arr: number[]): boolean {
+
    let obj = {};
+   // store in obj 
     for(let i=0; i<arr.length; i++){
         obj[arr[i]] = (obj[arr[i]] || 0) + 1;
-    } // [1,2,2,1,1,3] 
-    // obj = { 1 : 3 ,  2 : 2, 3 : 1 }
+    } 
+
+    // filter dups 
     var mySet = new Set([...Object.values(obj)])
 
+   // if length is diff , then there's dups 
     return mySet.size === Object.values(obj).length
 };
