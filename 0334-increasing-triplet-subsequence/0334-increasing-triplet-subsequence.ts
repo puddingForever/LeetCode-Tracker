1d4c21bef053013e8fb8dc4516ccd9afbfaec653
BytePudding <developerjieun94@gmail.com>
2025-08-28 10:23:52 +0900

    Time: 5 ms (30.51%), Space: 75.3 MB (50.55%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.ts
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.ts
@@ -1,16 +1,17 @@
 function increasingTriplet(nums: number[]): boolean {
-   let start = Infinity;
-   let second = Infinity;
-
-   for(let num of nums){
-    if(num <= start){
-        start = num;
-    }else if(num <= second){
-        second = num;
-    }else{
-        return true;
-    }
-   }
     
-    return false;
+  let first = Infinity , second = Infinity ;
+
+     for(let num of nums){
+        if(first >= num){
+            first = num;  
+        }else if(second >= num){
+            second = num; 
+        }else{
+	          return true;
+       }
+    }  
+
+  return false;
+
 };
