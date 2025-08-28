f0d9d6e2cdb49a2911ad045e9945d3b17c3e13f3
BytePudding <developerjieun94@gmail.com>
2025-08-28 09:32:30 +0900

    Time: 7 ms (86.39%), Space: 62.6 MB (78.5%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.ts
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.ts
@@ -1,32 +1,12 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    
+    // filter out only duplicates 
+    const only1 = [...set1].filter(num => !set2.has(num));
+    const only2 = [...set2].filter(num => !set1.has(num));
 
-      let obj = {};
-      let obj2 = {}
+    return [only1,only2]
+    
 
-    // Store all elements from nums1 in obj
-    for(let i=0; i<nums1.length; i++){
-        obj[nums1[i]] = nums1[i];
-    }
-    
-    // If an element of nums2 exists in obj, delete it
-    for(let i=0; i<nums2.length; i++){
-        if(obj[nums2[i]] || nums2[i] === 0){
-            delete obj[nums2[i]]
-        }
-    }
-    
-   // Store all elements from nums2 in obj2   
-    for(let i=0; i<nums2.length; i++){
-        obj2[nums2[i]] = nums2[i];
-    }
-    
-   // If an element of nums1 exists in obj2, delete it
-    for(let i=0; i<nums1.length; i++){
-        if(obj2[nums1[i]] || obj2[nums1[i]] === 0){
-            delete obj2[nums1[i]];
-        }
-    }
-    
-   // Now both obj and obj2 contain only distinct keys
-    return [ [...Object.keys(obj).map(Number)] , [...Object.keys(obj2).map(Number)] ];
 };
