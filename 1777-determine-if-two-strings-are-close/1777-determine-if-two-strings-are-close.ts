fc7832bf8d52c7bb098e552234f17d6bdfc3fb77
BytePudding <developerjieun94@gmail.com>
2025-09-01 13:35:58 +0900

    Time: 74 ms (48.62%), Space: 66.3 MB (26.61%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.ts
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.ts
@@ -1,17 +1,27 @@
 function closeStrings(word1: string, word2: string): boolean {
- 
-    let charCounts1 = word1.split("")
-                           .reduce((acc,val) => acc.set( val, (acc.get(val) ?? 0 ) + 1 ),new Map<string,number>());
-    let charCounts2 = word2.split("")
-                           .reduce((acc,val) =>  acc.set( val, ( acc.get(val) ?? 0) + 1),new Map<string,number>());
+    const charCounts1 = word1
+                        .split("")
+                        .reduce(
+                            (acc, val) => acc.set(val, (acc.get(val) ?? 0) + 1),
+                            new Map<string, number>()
+                        );
 
+    const charCounts2 = word2
+                        .split("")
+                        .reduce(
+                            (acc, val) => acc.set(val, (acc.get(val) ?? 0) + 1),
+                            new Map<string, number>()
+                        );
 
-    if(charCounts1.size !== charCounts2.size || ![...charCounts1.keys()].every((char) => charCounts2.has(char))){
+    if (
+        charCounts1.size !== charCounts2.size ||
+        ![...charCounts1.keys()].every((char) => charCounts2.has(char))
+    ) {
         return false;
     }
 
-    let sortedArr1 = [...charCounts1.values()].sort();
-    let sortedArr2 = [...charCounts2.values()].sort();
+    const sortedArr1 = [...charCounts1.values()].sort();
+    const sortedArr2 = [...charCounts2.values()].sort();
 
-    return sortedArr1.every((val,index) => val === sortedArr2[index])
-};
+    return sortedArr1.every((val, index) => val === sortedArr2[index]);
+}
