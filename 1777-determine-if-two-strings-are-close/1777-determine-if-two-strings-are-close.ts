e00805c20f51d0ddb6896ebeea42d61130a8d5a8
BytePudding <developerjieun94@gmail.com>
2025-09-01 14:25:32 +0900

    Time: 765 ms (5.2%), Space: 74.9 MB (5.2%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.ts
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.ts
@@ -1,21 +1,19 @@
 function closeStrings(word1: string, word2: string): boolean {
     const charCounts1 = word1.split("")
-                        .reduce((acc, val) => acc.set(val, (acc.get(val) ?? 0) + 1),
-                            new Map<string, number>());
+                        .reduce((acc, val) => ({...acc, [val] : (acc[val] ?? 0 ) + 1}),{});
 
     const charCounts2 = word2.split("")
-                        .reduce((acc, val) => acc.set(val, (acc.get(val) ?? 0) + 1),
-                            new Map<string, number>());
+                        .reduce((acc, val) => ({...acc, [val] : (acc[val] ?? 0) + 1}),{});
 
     if (
-        charCounts1.size !== charCounts2.size ||
-        ![...charCounts1.keys()].every((char) => charCounts2.has(char))
+        Object.keys(charCounts1).length !==  Object.keys(charCounts2).length ||
+        ![...Object.keys(charCounts1)].every((char) => char in charCounts2)
     ) {
         return false;
     }
 
-    const sortedArr1 = [...charCounts1.values()].sort();
-    const sortedArr2 = [...charCounts2.values()].sort();
+    const sortedArr1 = [...Object.values(charCounts1)].sort();
+    const sortedArr2 = [...Object.values(charCounts2)].sort();
 
     return sortedArr1.every((val, index) => val === sortedArr2[index]);
 }
