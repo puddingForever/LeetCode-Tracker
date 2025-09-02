ffb59cd10686e26b990b64590c6fc3c6686e53be
BytePudding <developerjieun94@gmail.com>
2025-09-02 14:17:56 +0900

    Time: 33 ms (97.33%), Space: 55.9 MB (50.26%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -1,10 +1,16 @@
 function mergeAlternately(word1: string, word2: string): string {
-    let res:string[] = [];
-    
-    for(let i=0; i<Math.max(word1.length, word2.length); i++){
-        if(i < word1.length) res.push(word1[i]);
-        if(i < word2.length) res.push(word2[i]);
-    }
 
-    return res.join("")
+    if(word1.length === 1 && word2.length === 1){
+         return word1[0] + word2[0];
+     }else if(word1.length === 0 && word2.length > 0){
+         return word2;
+     }else if(word1.length > 0 && word2.length === 0 ){
+         return word1;
+     }
+    
+    // both of them have words more than 2 letters 
+    let front = word1[0] + word2[0];
+    return front + mergeAlternately(word1.slice(1),word2.slice(1));
+    
+
 };
