757fc74538bfb9a8bf122c4690320efc8f3202a3
BytePudding <developerjieun94@gmail.com>
2025-09-04 16:08:31 +0900

    Time: 0 ms (100%), Space: 58 MB (9.68%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.ts
@@ -0,0 +1,9 @@
+function findClosest(x: number, y: number, z: number): number {
+    // Math.abs 해서 크기가 작은것이 1 
+    let first = Math.abs(z-x);
+    let second = Math.abs(z-y);
+
+    if(first === second) return 0;
+    if(first < second) return 1;
+    if(second < first) return 2;
+};
