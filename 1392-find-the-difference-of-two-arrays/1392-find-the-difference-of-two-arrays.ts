d26857453246f0d3756af358a9a481c01f51d983
BytePudding <developerjieun94@gmail.com>
2025-09-05 10:29:17 +0900

    Time: 8 ms (77.07%), Space: 62.1 MB (87.48%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.ts
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.ts
@@ -1,7 +1,7 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
- const [only1,only2] = [new Set(nums1),new Set(nums2)];
+    const [only1,only2] = [new Set(nums1),new Set(nums2)];
 
- return [
-     [...only1].filter(num => !only2.delete(num)), [...only2]
- ]
+    return [
+        [...only1].filter(num => !only2.delete(num)), [...only2]
+    ]
 };
