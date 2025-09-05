3cdf54b67e0543fbf4f3d2f03c6ff9af8fe8126d
BytePudding <developerjieun94@gmail.com>
2025-09-05 10:29:07 +0900

    Time: 3 ms (100%), Space: 62.9 MB (66.86%) - LeetHub

--- 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.ts
+++ 1392-find-the-difference-of-two-arrays/1392-find-the-difference-of-two-arrays.ts
@@ -1,13 +1,7 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
- // 1. set으로 각각 넣은 후 
-  // 2. !set.has 인것만 array로 넣은 후  
-  // 3. 리턴한다.
-    const mySet1 = new Set(nums1);
-    const mySet2 = new Set(nums2);
+ const [only1,only2] = [new Set(nums1),new Set(nums2)];
 
-    let arr1 = [...mySet1].filter(num => !mySet2.has(num));
-    let arr2 = [...mySet2].filter(num => !mySet1.has(num));
-
-    return [arr1,arr2]
-
+ return [
+     [...only1].filter(num => !only2.delete(num)), [...only2]
+ ]
 };
