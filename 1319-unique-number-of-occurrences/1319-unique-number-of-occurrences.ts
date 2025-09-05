6b0fa59d93bf0d7c77258f95fd97942335656f6c
BytePudding <developerjieun94@gmail.com>
2025-09-05 10:49:23 +0900

    Time: 1 ms (67.25%), Space: 55.6 MB (70.96%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.ts
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.ts
@@ -1,11 +1,6 @@
 function uniqueOccurrences(arr: number[]): boolean {
-    // map에 넣은 후에 
-    // map의 길이와 , set에 넣어둔 후의 길이를 비교해서 같으면 true, 
-    // 아니면 false 
-    // 중복값이 나오면 , 값이 사라지기 때문에 
-    const myMap = arr.reduce((acc,curr) => acc.set(curr, ( acc.get(curr) || 0 ) + 1 )
+  const myMap = arr.reduce((acc,curr) => acc.set(curr, ( acc.get(curr) || 0 ) + 1 )
                              ,new Map())   
-    const mySet = new Set([...myMap.values()]);
 
-    return myMap.size === mySet.size
+    return myMap.size ===  new Set(myMap.values()).size
 };
