1526b71f8522903c4d0e93da850c0ea4460f37b1
BytePudding <developerjieun94@gmail.com>
2025-09-11 09:27:31 +0900

    Time: 2 ms (82.45%), Space: 69.5 MB (56.55%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,18 +1,16 @@
 function findMaxAverage(nums: number[], k: number): number {
-
-    let max = 0;
+// [1,12,-5,-6,50,3] 4 
+    let max = 0; 
     for(let i=0; i<k; i++){
         max += nums[i];
-    }
-   // console.log(max) 2 // 1,12,-5,-6
-    let tmp = max; 
-    for(let i=k; i<nums.length; i++){ 
-        tmp = tmp + nums[i] - nums[i-k]; // 52
+    }    
+    let tmp = max;
+    for(let j=k; j<nums.length; j++){
+        tmp += nums[j] - nums[j-k]
         max = Math.max(max,tmp);
     }
-
+        
     return max/k;
-
     
       
 };
