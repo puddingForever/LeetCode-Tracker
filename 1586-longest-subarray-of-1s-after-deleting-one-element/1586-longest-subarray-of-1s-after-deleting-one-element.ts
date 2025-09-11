8781256a191999b30b62075bd8506584f15098d4
BytePudding <developerjieun94@gmail.com>
2025-09-11 10:53:34 +0900

    Time: 0 ms (100%), Space: 62.1 MB (23.89%) - LeetHub

--- 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.ts
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/1586-longest-subarray-of-1s-after-deleting-one-element.ts
@@ -1,18 +1,19 @@
 function longestSubarray(nums: number[]): number {
-    let left = 0;
-    let maxLen = 0;
-    let zeroCnt = 0;
-    for(let right = 0; right < nums.length; right++){
-        if(nums[right] === 0) zeroCnt++;
 
+     let left = 0 , zeroCnt = 0, max = 0; 
+    for(let right = 0 ; right < nums.length; right ++ ){
+        if(nums[right] === 0) zeroCnt++;
+       
         while(zeroCnt > 1){
-            if(nums[left] === 0) zeroCnt--;
+            if(nums[left] === 0) zeroCnt --; 
             left++;
         }
 
-        maxLen = Math.max(maxLen,right - left);
+
+        max = Math.max(max, right - left);
+        
     }
 
-    return maxLen;
+    return max;
     
 };
