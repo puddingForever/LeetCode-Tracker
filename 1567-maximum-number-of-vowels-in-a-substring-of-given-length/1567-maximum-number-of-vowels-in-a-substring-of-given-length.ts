9046fac8057ea2da22afedaba3b13f66c309a78c
BytePudding <developerjieun94@gmail.com>
2025-09-11 10:06:58 +0900

    Time: 23 ms (16.06%), Space: 58.2 MB (63.86%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.ts
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -1,22 +1,18 @@
 function maxVowels(s: string, k: number): number {
    const mySet = new Set(['a','e','i','o','u'])
-    let max = 0; 
+   let max = 0; 
    for(let i=0; i<k; i++){ 
        if(mySet.has(s[i])){  
            max++;
        }
    }
-    
-  // 둘다 모음이 아니면 계산필요 X 
-    // 내가 모음이고 이전이 모음이면 계산 필요 X ( + 1 하고 - 1 하니까 )
-    // 내가 모음이고 이전이 모음이 아니면 + 1  
+
    let tmp = max; 
    for(let j=k; j<s.length; j++){
        if(mySet.has(s[j]) && !mySet.has(s[j-k])){
             tmp = tmp + 1; 
         }else if(!mySet.has(s[j])  && mySet.has(s[j-k])){
             tmp = tmp - 1;
-           console.log(s[j], tmp)
         }
         max = Math.max(tmp, max);
     }
