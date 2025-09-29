6805f7d47f4e92af7673586954e6ef3109454701
BytePudding <developerjieun94@gmail.com>
2025-09-29 09:14:46 +0900

    Time: 41 ms (81.67%), Space: 55.8 MB (5.57%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.ts
+++ 1013-fibonacci-number/1013-fibonacci-number.ts
@@ -1,6 +1,6 @@
-function fib(n: number): number {
-        let memo = {};
+function fib(n: number, memo:Record<number,number> = {} ): number {
         if(n <= 1) return n;
-        memo[n] = fib(n-1) + fib(n-2);
+        if(memo[n] !== undefined) return memo[n];
+        memo[n] = fib(n-1,memo) + fib(n-2,memo);
         return memo[n];
 };
