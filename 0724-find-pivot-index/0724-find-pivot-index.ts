9124f1a78d4d11bc5e3c2f9202b0a3e60361c9c1
BytePudding <developerjieun94@gmail.com>
2025-09-29 09:47:13 +0900

    Time: 2 ms (64.23%), Space: 57.9 MB (84.36%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.ts
+++ 0724-find-pivot-index/0724-find-pivot-index.ts
@@ -1,13 +1,11 @@
 function pivotIndex(nums: number[]): number {
-    let total = nums.reduce((acc,val) => acc+val ,0);
-    let leftSum = 0;
+    let total = nums.reduce((acc,val) => acc + val,0);
+    let leftSum=0,rightSum=0;
+ 
     for(let i=0; i<nums.length; i++){
-        let rightSum = total - leftSum - nums[i];
-        if(leftSum === rightSum){
-            return i;
-        }
-        leftSum += nums[i];
+        rightSum = total - leftSum - nums[i]; // 0 
+        if(leftSum === rightSum) return i;
+        leftSum += nums[i]; 
     }
-
     return -1;
 };
