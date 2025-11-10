c225e3230446e462d35105a329f93d2faebeacf6
BytePudding <developerjieun94@gmail.com>
2025-11-10 14:32:42 +0900

    Time: 8 ms (50.83%), Space: 68.7 MB (90.92%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,20 +1,19 @@
 function productExceptSelf(nums: number[]): number[] {
-     let len = nums.length;
-    let leftArr = new Array(len);
-    let answer = new Array(len);
-    let prod = 1;
-   
-    for(let i=0; i<len; i++){ // 1 1 2 6
-      leftArr[i] = prod;
-       prod = prod * nums[i]
+   let len = nums.length;
+   let leftArr = new Array(len);
+   let answerArr = new Array(len);
+   let prod = 1;
+
+    for(let i=0; i<nums.length; i++){
+        leftArr[i] = prod;
+        prod = prod * nums[i];
     }
     prod = 1;
-    for(let i=len-1; i>=0; i--){
-       answer[i] = leftArr[i] * prod;
-       prod = prod * nums[i];
+    for(let i= nums.length-1; i>=0; i--){
+        answerArr[i] = leftArr[i] * prod;
+        prod = prod * nums[i];
     }
-
-   return answer;
-   
+    
+    return answerArr;
     
 };
